09445ff535020676fa7a47afe346dd290b1fba38
CaioMicael <142358332+CaioMicael@users.noreply.github.com>
2025-01-09 21:57:25 -0300

    .

--- model/teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace model;
-require_once 'ClassModelEstado.php';
-
-$teste = new ClassModelEstado;
-$teste->setAttributeModel('estadoTeste','pais');
-echo $teste->isEstadoCadastrado();
-
-?>
