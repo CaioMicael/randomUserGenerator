4882f31550af7c45956647e1f8cbfceb4447b7f3
Caio Micael <contatocaiomicael@gmail.com>
2025-03-09 18:38:41 -0300

    Implementado controllerPais conforme novos métodos estruturais.

--- controller/ClassControllerPais.php
+++ controller/ClassControllerPais.php
@@ -3,8 +3,11 @@ namespace controller;
 
 use lib\estClassController;
 use lib\enum\estClassEnumAcoes;
+use lib\enum\estClassEnumMensagensWebbased;
 use model\ClassModelPais;
 use view\ClassViewManutencaoPais;
+use Exception;
+use Throwable;
 
 require_once '../autoload.php';
 
@@ -35,7 +38,7 @@ class ClassControllerPais extends estClassController {
      * 
      * @return array
      */
-    public function getDadosTratadoConsultaPaisController() {
+    public function getDadosConsultaPaisController() {
         return $this->trataDadosConsultaChave($this->getMapaChaveColunasPais(),$this->getDadosConsultaPaisFromModel());
     }
     
@@ -54,11 +57,21 @@ class ClassControllerPais extends estClassController {
     }
 
 
-/***************************************************************************************************************************/
-/*************************************             MÉTODOS DE FORMULÁRIO                 ***********************************/
-/***************************************************************************************************************************/
+    /**
+     * Este método retorna um array com as tipagens
+     * do nome da coluna e o type HTML e atributos.
+     * 
+     * @return array
+     */
+    public function getTipagemCamposToHtml() {
+        return [
+            "Código do País"   => ["name" => "pais.codigo"  ,"type"   => "number", "required" => "required", "value" => "", "disabled" =>         "", "lupa" => "Pais"],         
+            "Nome do País"     => ["name" => "pais.nome"    ,"type"   => "text"  , "required" => "required", "value" => "", "disabled" =>         "", "lupa" => false]        
+        ];
+    }
 
-
+/***************************************** Métodos de formulário *************************************************
+ *****************************************************************************************************************/
     /**
      * Este método retorna a view de País com o botão
      * de selecionar registro.
@@ -70,6 +83,80 @@ class ClassControllerPais extends estClassController {
         return $this->viewPais->getConsultaPaisView(
             [estClassEnumAcoes::SELECIONAR]
         );
+    }
+
+
+        /**
+     * Este método chama a tela de inclusão de País da view.
+     * 
+     * @return view
+     */
+    public function getTelaIncluirPais() {
+        $this->viewPais = new ClassViewManutencaoPais;
+        return $this->viewPais->getTelaIncluirPais($this->getTipagemCamposToHtml());
+    }
+
+
+    /**
+     * Este método retorna a tela de alterar País da view.
+     * @param array $aDados
+     * @return view
+     */
+    public function getTelaAlterarPais($aDados) {
+        try {
+            $this->viewPais = new ClassViewManutencaoPais;
+            return $this->viewPais->getTelaAlterarPais($this->getTipagemCamposToHtml(), $aDados['dados']);
+        }
+        catch (Throwable $e) {
+            return json_encode($this->retornaExceptionFrontEnd($e));
+        }
+    }
+
+
+    /**
+     * Recebe os dados de inclusão, trata e envia ao model.
+     * 
+     * @param array $aDados
+     */
+    public function processaDadosIncluirPais($aDados) {
+        $iPaisCodigo = filter_var($aDados['dados']['pais.codigo'], FILTER_SANITIZE_NUMBER_INT);
+        $sPaisNome   = filter_var($aDados['dados']['pais.nome'], FILTER_SANITIZE_NUMBER_INT);
+
+        try {
+            $this->modelPais->processaDadosIncluir($iPaisCodigo, $sPaisNome);
+            return json_encode($this->retornaIncluidoSucessoFrontEnd(estClassEnumMensagensWebbased::webbased006->value));
+        }
+        catch(Exception $e) {
+           return json_encode($this->retornaExceptionFrontEnd($e));
+        }  
+    }
+
+
+    /**
+     * Este método recebe o dado selecionado para exclusão e
+     * chama o Model para realizar a exclusão.
+     */
+    public function processaDadosExcluirPais($aDados) {
+        $this->modelPais->processaDadosExcluir($aDados["dados"]);
+        return json_encode($this->retornaIncluidoSucessoFrontEnd(estClassEnumMensagensWebbased::webbased013->value));
+    }
+
+
+    /**
+     * Este método recebe o dado selecionado para alteração e
+     * chama o Model para realizar a alteração.
+     */
+    public function processaDadosAlterarPais($aDados) {
+        try {
+            $this->modelPais->processaDadosAlterar(
+                $aDados["dados"]["pais.codigo"],
+                $aDados["dados"]["pais.nome"]
+            );
+            return json_encode($this->retornaIncluidoSucessoFrontEnd(estClassEnumMensagensWebbased::webbased014->value));
+        }
+        catch (Exception $e) {
+            return json_encode($this->retornaExceptionFrontEnd($e));
+        }
     }
 
 
