54ef681534b9ec36bab2c1c4abf9a026695ac11e
CaioMicael <contatocaiomicael@gmail.com>
2025-03-26 18:17:27 -0300

    Alterado view de Pessoa para usar a nova classe de campos na alteração

--- view/ClassViewManutencaoPessoa.php
+++ view/ClassViewManutencaoPessoa.php
@@ -31,6 +31,27 @@ class ClassViewManutencaoPessoa extends estClassViewManutencao {
     public function getConsultaPessoaView($aAcoes) {
         return $this->getConsulta($this->controllerPessoa->getDadosConsultaPessoa(), $aAcoes);
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getTelaAlteracao($aCampos, $aDados){
+        return $this->getTelaAlteracao($this->getCamposAlteracao(), $aDados);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getCamposAlteracao() {
+        $this->addCampo()->setNameCampo('pessoa.seed')->setNomeLabel('Seed')->setTipagem('text')->setDisabled();
+        $this->addCampo()->setNameCampo('pessoa.codigo')->setNomeLabel('Código')->setTipagem('text')->setDisabled();
+        $this->addCampo()->setNameCampo('pessoa.nome')->setNomeLabel('Nome')->setTipagem('text')->setRequired();
+        $this->addCampo()->setNameCampo('pessoa.email')->setNomeLabel('E-mail')->setTipagem('email')->setRequired();
+        $this->addCampo()->setNameCampo('pessoa.telefone')->setNomeLabel('Telefone')->setTipagem('text')->setRequired();
+        $this->addCampo()->setNameCampo('pessoa.celular')->setNomeLabel('Celular')->setTipagem('text')->setRequired();
+    }
 }
-echo '<script type="module" src="viewComportamento/classViewComportamentoPessoa.js"></script>';
+if ($_SERVER["REQUEST_URI"] == '/randomusergenerator/view/ClassViewManutencaoPessoa.php') {
+    echo '<script type="module" src="viewComportamento/ClassViewComportamentoPessoa.js"></script>';
+}
 ?>
